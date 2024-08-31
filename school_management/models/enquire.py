6fd76025b88174b23f31233de59e872064999553
ManoDurga1 <durga@sidmectech.in>
2024-08-31 16:57:17 +0530

    school management

--- school_management/models/enquire.py
+++ school_management/models/enquire.py
@@ -2,7 +2,7 @@ from odoo import api,models,fields
 
 class Enquire(models.Model):
     _name = "scl.enquire"
-    _description = "enquire about school management"
+    _description = "Admissions Open"
     parent_name = fields.Char(string="Parent Name")
     parent_job = fields.Char(string="Parent Job")
     parent_no= fields.Char(string="Parent No")
@@ -14,6 +14,16 @@ class Enquire(models.Model):
 
     def action_join(self):
         self.status='admit'
+        var=self.env["school.student"]
+        for rec in self:
+            var.create({
+                'student_name':rec.student_name,
+                'gaurdian_name':rec.parent_name,
+                'phone_number':rec.parent_no,
+                'age':rec.student_age,
+                'standard':rec.standard
+
+            })
 
 
 
